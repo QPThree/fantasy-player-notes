5d6f546105bc02746f354a5e972d2d5e5f44e002
QPThree <youngqp3@gmail.com>
2021-06-25 19:04:09 -0700

    added local storage functions

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ function CreateNote(playerName, position, team, text){
   this.text = text;
 
   allNotes.push(this);
-  console.log(this);
+  addToLocalStorage('allNotes', allNotes);
 }
 
 
@@ -23,16 +23,38 @@ function CreateNote(playerName, position, team, text){
 
 function renderCard(obj){
   console.log('card rendered');
-  let allCards = document.getElementsByClassName('allCards');
+  let allCards = document.getElementById('allCards');
   let div = document.createElement('div');
   let h1 = document.createElement('h1');
   let h2 = document.createElement('h2');
-  div.className = 'card'; 
+  let h3 = document.createElement('h3');
+  let p = document.createElement('p');
 
+  div.className = 'card'; 
+  allCards.appendChild(div);
   h1.textContent = obj.name;
   h2.textContent = obj.position;
+  h3.textContent = obj.team;
+  p.textContent = obj.text;
+
+  div.appendChild(h1);
+  div.appendChild(h2);
+  div.appendChild(h3);
+  div.appendChild(p);
+
+  
+
 }
 
+function addToLocalStorage(key, item) {
+  let stringifiedItem = JSON.stringify(item);
+  localStorage.setItem(key, stringifiedItem);
+}
+
+function getFromLocalStorage(key) {
+  let returnedItem = JSON.parse(localStorage.getItem(key));
+  return returnedItem;
+}
 //event handlers
 function handleCreateNote(event){
   event.preventDefault();
@@ -41,6 +63,7 @@ function handleCreateNote(event){
   let team = event.target.createTeam.value;
   let text = event.target.createText.value;
   renderCard(new CreateNote(playerName, position, team, text));
+  console.log(allNotes);
 }
 
 //event listeners
@@ -48,3 +71,8 @@ createNoteForm.addEventListener('submit', handleCreateNote);
 
 //proof of lifes///
 // new CreateNote('Tom Brady', 'QB', 'NE', 'He should be drafted high');
+//render on run
+
+if (getFromLocalStorage('allNotes')){
+  allNotes = getFromLocalStorage('allNotes');
+}
