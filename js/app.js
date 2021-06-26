88b9a1c5464d6abaea8ca509aac8ce2070e920ba
QPThree <youngqp3@gmail.com>
2021-06-25 17:35:41 -0700

    have constructor function working with form and create button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 'use strict';
+//globals variables
+const allNotes = [];
+//footholds into DOM
+let createNoteForm = document.getElementById('create-note-form');
+//constructor functions
+
+function CreateNote(playerName, position, team, text){
+  this.playerName = playerName;
+  this.position = position;
+  this.team = team;
+  this.text = text;
+
+  allNotes.push(this);
+  console.log(this);
+}
+
+
+//prototype methods
+
+
+//functions
+
+
+
+//event handlers
+function handleCreateNote(event){
+  event.preventDefault();
+  let playerName = event.target.createPlayerName.value;
+  let position = event.target.createPosition.value;
+  let team = event.target.createTeam.value;
+  let text = event.target.createText.value;
+  new CreateNote(playerName, position, team, text);
+}
+
+//event listeners
+createNoteForm.addEventListener('submit', handleCreateNote);
+
+//proof of lifes///
+// new CreateNote('Tom Brady', 'QB', 'NE', 'He should be drafted high');
